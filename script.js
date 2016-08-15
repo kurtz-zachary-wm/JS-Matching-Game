5db0d2a49a5aac7b91ebd310a3f63a40aa33be08
Start Coding Student 2 <session2@START-Coding-MBP-13.local>
2016-08-15 14:33:51 -0700

    Created part3 HTML page, and created function to work on leftside.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var numberOfFaces = 5;
 var leftSide = document.getElementById('leftSide');
+var rightSide = document.getElementById('rightSide');
 
 function generateFaces() {
     for (var i = 0; i < numberOfFaces; i ++) {
@@ -11,5 +12,9 @@ function generateFaces() {
         img.style.top = Math.random() * 400 + 'px';
         leftSide.appendChild(img);
     }
+    var leftSideClone = leftSide.cloneNode(true);
+    leftSideClone.removeChild(leftSideClone.lastChild);
+    rightSide.appendChild(leftSideClone);
 }
 generateFaces();
+
