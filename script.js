1fe407b7bdefb3d5796c663adc28964471c6a36a
Start Coding Student 2 <session2@START-Coding-MBP-13.local>
2016-08-15 14:09:49 -0700

    Transferred files over from previous project.

---
+++ script.js
@@ -0,0 +1,9 @@
+var numberOfFaces = 5;
+var leftSide = document.getElementById('leftSide');
+    document.appendChild('keenan_allen.png');
+
+function generateFaces() {
+    var img = document.createElement('img');
+
+
+}
